e6620c83af0d628107149cc994cd4935530b3eb1
hariharan s <hariharans.developer@gmail.com>
2025-03-31 22:32:40 +0530

    bug fix

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from Vercel!" });
 });
 
